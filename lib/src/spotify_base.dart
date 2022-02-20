c9b372eda3603be3e465851151ebed8aec473cf2
Ian <ian@straightforward.it>
2022-02-20 16:47:18 +1300

    removed AlbumsMe from spotify_base

--- lib/src/spotify_base.dart
+++ lib/src/spotify_base.dart
@@ -15,10 +15,6 @@ abstract class SpotifyApiBase {
   Artists get artists => _artists;
   late Albums _albums;
   Albums get albums => _albums;
-//Ian added AlbumsMe
-  late AlbumsMe _albumsme;
-  AlbumsMe get AlbumSaved => _albumsme;
-
   late Tracks _tracks;
   Tracks get tracks => _tracks;
   late Playlists _playlists;
